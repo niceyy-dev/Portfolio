c98935861f466152595f79de19db14b6c41ce560
niceyy-dev <zhanais.dev@gmail.com>
2025-05-16 14:46:50 +0900

    add github action

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
   /* config options here */
 };
 
