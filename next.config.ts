50f051a6f604b9fcd80839b88caa325942562e1e
niceyy-dev <zhanais.dev@gmail.com>
2025-05-17 11:46:39 +0900

    fix: remove next export (Next 15+)

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio/",
+  // basePath: "/Portfolio",
+  // assetPrefix: "/Portfolio/",
   images: {
     unoptimized: true,
   },
